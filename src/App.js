d4dc48816746ed4d5a3cb69d76794bd198a42381
Aarre Suvanto <a1700390@myy.haaga-helia.fi>
2020-02-07 18:52:38 +0200

    app renders all restaurant names

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import './App.css';
 const restaurantData = require('./restaurants.json')
 
 function App() {
+  const [ restaurants, setRestaurants ] = useState(restaurantData.restaurants)
+
+  console.log(restaurants)
+
+  const restaurantNamesListed = restaurants.map((restaurant, index) => {
+    return (
+          <li key={index}>{restaurant.name}</li>
+    )
+  })
+
   return (
     <div className="App">
-      <h3>hodl</h3>
+      <ul>
+        {restaurantNamesListed}
+      </ul>
     </div>
   );
 }
